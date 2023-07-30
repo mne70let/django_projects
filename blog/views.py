a9ae2ce40733e8b11eb49d24f7a58bef26d48781
mne70let <pesotskiy.sergey@gmail.com>
2023-07-30 18:17:39 +0000

    3rd commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post #import model. Dot means same folder/app
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.order_by('published_date')
+    # render with additional model derived input for template
+    return render(request, 'blog/post_list.html', {'posts': posts})
